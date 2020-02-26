81983ca62cbc86f2b33cd4ce20518f53fb9cbb5d
Pranav Garg <pranavgarg@edlab-mail.cs.umass.edu>
2020-02-26 15:42:08 -0500

    memory.c completed

--- memory.c
+++ memory.c
@@ -36,10 +36,10 @@ int main () {
    free(arr);
 
    //TODO: remove the comment below after finished the functions
-   /*
+   
    int** arr_2d = build_2darr(5);
-   print_2darr(arr_2d);
-   */
+   print_2darr(arr_2d, 5);
+  
 
    return(0);
 }
@@ -62,10 +62,25 @@ void print_arr(int *arr, int n){
 */
 int** build_2darr(int n){
    //TODO
-   return NULL;
+   int** arr = (int **)malloc(n*sizeof(int *));
+   for(int i=0; i<n; i++) {
+	arr[i] = (int *)malloc((i+1)*sizeof(int));
+   }
+   for(int i=0; i<n; i++) {
+        for(int j=0; j<=i; j++) {
+	     arr[i][j]=i;
+        }
+   }
+   return arr;
 }
 
 // print the 2d array you build previously
 void print_2darr(int** arr, int n){
    //TODO
+   for(int i=0; i<n; i++) {
+	for(int j=0; j<=i; j++) {
+		printf("%d ", arr[i][j]);
+	}
+	printf("%s", "\n");
+   }  
 }
